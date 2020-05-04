4c2d141ff9c9ef233470cebd8f97f6d4410ed2bd
Pavel Shramov <psha@nguni.psha.org.ru>
2020-05-04 11:12:47 +0300

    logic: Simple forward logic

---
+++ src/logic/forward.cc
@@ -0,0 +1,40 @@
+#include "tll/channel/logic.h"
+
+class Forward : public tll::LogicBase<Forward>
+{
+	tll::Channel * _input = nullptr;
+	tll::Channel * _output = nullptr;
+ public:
+	static constexpr std::string_view param_prefix() { return "logic"; }
+	static constexpr std::string_view impl_protocol() { return "logic+forward"; }
+
+	int _init(const tll::Channel::Url &, tll::Channel *master);
+
+	int logic(const tll::Channel * c, const tll_msg_t *msg);
+};
+
+int Forward::_init(const tll::Channel::Url &url, tll::Channel *)
+{
+	auto i = _channels.find("input");
+	auto o = _channels.find("output");
+	if (i == _channels.end()) return _log.fail(EINVAL, "No input channels");
+	if (o == _channels.end()) return _log.fail(EINVAL, "No output channels");
+	if (i->second.size() != 1) return _log.fail(EINVAL, "Need exactly one input, got {}", i->second.size());
+	if (o->second.size() != 1) return _log.fail(EINVAL, "Need exactly one output, got {}", i->second.size());
+	_input = i->second.front();
+	_output = o->second.front();
+	return 0;
+}
+
+int Forward::logic(const tll::Channel * c, const tll_msg_t *msg)
+{
+	if (msg->type != TLL_MESSAGE_DATA)
+		return 0;
+	if (c == _input)
+		_output->post(msg);
+	return 0;
+}
+
+TLL_DEFINE_IMPL(Forward);
+
+auto channel_module = tll::make_channel_module<Forward>();
