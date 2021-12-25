a54131a2f4781108a7fa665e935ac3442dc2acbf
Pavel Shramov <psha@nguni.psha.org.ru>
2021-12-25 19:55:57 +0300

    python: Added tmp_path fixture compatible with older pytest

--- python/test/conftest.py
+++ python/test/conftest.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 # vim: sts=4 sw=4 et
 
+import pytest
+
+import pathlib
+import tempfile
+
 import tll.logger
 
 tll.logger.init()
+
+version = tuple([int(x) for x in pytest.__version__.split('.')[:2]])
+
+if version < (3, 9):
+    @pytest.fixture
+    def tmp_path():
+        with tempfile.TemporaryDirectory() as tmp:
+            yield pathlib.Path(tmp)
