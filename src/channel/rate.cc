bb2947ebc6d15ab6c4bbb3a202241f23db130602
Pavel Shramov <psha@nguni.psha.org.ru>
2023-05-30 08:31:27 +0300

    rate: Use common code to merge control scheme

--- src/channel/rate.cc
+++ src/channel/rate.cc
@@ -11,68 +11,14 @@
 #include "tll/util/size.h"
 #include "tll/util/time.h"
 
+#include "tll/scheme/merge.h"
+
 #include "tll/scheme/channel/timer.h"
 #include "tll/channel/tcp-client-scheme.h"
 
 using namespace tll::channel;
 
 TLL_DEFINE_IMPL(Rate);
-
-namespace {
-bool intersects(const tll::Scheme * s0, const tll::Scheme * s1)
-{
-	if (!s1)
-		return false;
-	for (auto & m0 : tll::util::list_wrap(s0->messages)) {
-		if (s1->lookup(m0.name))
-			return true;
-	}
-	return false;
-}
-
-bool contains(const tll::Scheme * outer, const tll::Scheme * inner)
-{
-	if (!inner)
-		return true;
-	for (auto & mi : tll::util::list_wrap(inner->messages)) {
-		if (!outer->lookup(mi.name))
-			return false;
-	}
-	return true;
-}
-
-const tll::Scheme * merge(tll::channel::Context ctx, std::string_view s0_string, const tll::Scheme * s1)
-{
-	tll::scheme::ConstSchemePtr s0(ctx.scheme_load(s0_string));
-	if (!s1)
-		return s0.release();
-
-	if (!intersects(s0.get(), s1)) {
-		auto s1_string = tll_scheme_dump(s1, "yamls+gz");
-		auto merged = fmt::format("yamls://[{{name: '', import: ['{}', '{}']}}]", s0_string, s1_string);
-		free(s1_string);
-		return ctx.scheme_load(merged);
-	}
-
-	for (auto & m0 : tll::util::list_wrap(s0->messages)) {
-		auto m1 = s1->lookup(m0.name);
-		if (!m1)
-			continue;
-
-		if (m1->msgid != m0.msgid)
-			return nullptr;
-		if (m1->size != m0.size)
-			return nullptr;
-	}
-
-	if (contains(s0.get(), s1))
-		return s0.release();
-	if (contains(s1, s0.get()))
-		return s1;
-	return nullptr;
-
-}
-}
 
 int Rate::_init(const Channel::Url &url, tll::Channel *master)
 {
@@ -99,9 +45,10 @@ int Rate::_init(const Channel::Url &url, tll::Channel *master)
 	if (_conf.speed == 0) return _log.fail(EINVAL, "Zero speed");
 	if (_conf.limit <= 0) return _log.fail(EINVAL, "Invalid window size: {}", _conf.limit);
 
-	_scheme_control.reset(merge(context(), tcp_client_scheme::scheme_string, _child->scheme(TLL_MESSAGE_CONTROL)));
-	if (!_scheme_control.get())
-		return _log.fail(EINVAL, "Failed to load control scheme");
+	auto r = tll::scheme::merge({context().scheme_load(tcp_client_scheme::scheme_string), _child->scheme(TLL_MESSAGE_CONTROL)});
+	if (!r)
+		return _log.fail(EINVAL, "Failed to merge control scheme: {}", r.error());
+	_scheme_control.reset(*r);
 
 	auto curl = this->child_url_parse("timer://;clock=realtime", "timer");
 	if (!curl)
