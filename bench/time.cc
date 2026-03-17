dff4a5e1302e2f2b725e032ea410370fc66b1029
Pavel Shramov <psha@nguni.psha.org.ru>
2026-03-17 16:42:01 +0300

    bench: Measure time of CPU clock functions

--- bench/time.cc
+++ bench/time.cc
@@ -5,22 +5,27 @@
 
 #include <time.h>
 #include <sys/time.h>
+#include <sys/resource.h>
 
 using namespace tll::bench;
 
 using namespace std::chrono;
+
+nanoseconds struct2ns(const struct timespec &ts)
+{
+	return seconds(ts.tv_sec) + nanoseconds(ts.tv_nsec);
+}
+
+nanoseconds struct2ns(const struct timeval &ts)
+{
+	return seconds(ts.tv_sec) + microseconds(ts.tv_usec);
+}
+
 nanoseconds cgt(clockid_t clock)
 {
 	timespec ts = {};
 	clock_gettime(clock, &ts);
-	return seconds(ts.tv_sec) + nanoseconds(ts.tv_nsec);
-}
-
-nanoseconds gtod()
-{
-	timeval ts = {};
-	gettimeofday(&ts, 0);
-	return seconds(ts.tv_sec) + microseconds(ts.tv_usec);
+	return struct2ns(ts);
 }
 
 nanoseconds ftime() { return seconds(time(0)); }
@@ -34,8 +39,11 @@ int main()
 	timeit(count, "hrt::now", high_resolution_clock::now);
 	timeit(count, "clock_gettime(REALTIME)", cgt, CLOCK_REALTIME);
 	timeit(count, "clock_gettime(MONOTONIC)", cgt, CLOCK_MONOTONIC);
-	timeit(count, "gettimeofday", gtod);
-	timeit(count, "time", ftime);
+	timeit(count, "clock_gettime(PROCESS_CPUTIME)", cgt, CLOCK_PROCESS_CPUTIME_ID);
+	timeit(count, "clock_gettime(THREAD_CPUTIME)", cgt, CLOCK_THREAD_CPUTIME_ID);
+	timeit(count, "gettimeofday", [] { timeval v = {}; gettimeofday(&v, 0); return struct2ns(v); });
+	timeit(count, "getrusage", [] { rusage v = {}; getrusage(RUSAGE_SELF, &v); return struct2ns(v.ru_utime); });
+	timeit(count, "time", [] { return nanoseconds(seconds(time(0))); } );
 	timeit(count, "tll::time::now", tll::time::now);
 
 	return 0;
